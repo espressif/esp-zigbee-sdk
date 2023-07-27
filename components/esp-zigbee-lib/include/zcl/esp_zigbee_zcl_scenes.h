992d2f5b99d09ff49d4d820bb9dc59b1d40be6d4
xieqinan <xieqinan@espressif.com>
2023-07-27 19:55:36 +0800

    esp-zigbee-sdk: release/v0.7.2(e1472baa)
    * Add security network key getting API
    * Add zdo bind table request command
    * Add more zcl color control command support
    * Enlarge mac rx queue size
    * Support more endpoints on Zigbee device
    * Support more value types of custom cluster
    * Add scene cluster view command response
    * Add scene cluster for prompts
    * Optimize the scene cluster implementation
    
    Closes: https://github.com/espressif/esp-zigbee-sdk/issues/51
    Closes: https://github.com/espressif/esp-zigbee-sdk/issues/53

--- components/esp-zigbee-lib/include/zcl/esp_zigbee_zcl_scenes.h
+++ components/esp-zigbee-lib/include/zcl/esp_zigbee_zcl_scenes.h
@@ -52,6 +52,21 @@ typedef enum {
     ESP_ZB_ZCL_CMD_SCENES_COPY_SCENE            = 0x42,         /*!< The Copy Scene command allows a device to efficiently copy scenes from one group/scene identifier pair to another group/scene identifier pair. */
 } esp_zb_zcl_scenes_cmd_id_t;
 
+/** @brief Response command identifiers for Scenes Cluster
+ */
+typedef enum {
+    ESP_ZB_ZCL_CMD_SCENES_ADD_SCENE_RESPONSE            = 0x00,   /*!< Add scene response */
+    ESP_ZB_ZCL_CMD_SCENES_VIEW_SCENE_RESPONSE           = 0x01,   /*!< View scene response */
+    ESP_ZB_ZCL_CMD_SCENES_REMOVE_SCENE_RESPONSE         = 0x02,   /*!< Remove scene response */
+    ESP_ZB_ZCL_CMD_SCENES_REMOVE_ALL_SCENES_RESPONSE    = 0x03,   /*!< Remove all scenes response */
+    ESP_ZB_ZCL_CMD_SCENES_STORE_SCENE_RESPONSE          = 0x04,   /*!< Store scene response */
+    ESP_ZB_ZCL_CMD_SCENES_GET_SCENE_MEMBERSHIP_RESPONSE = 0x06,   /*!< Get scene membership response */
+    ESP_ZB_ZCL_CMD_SCENES_ENHANCED_ADD_SCENE_RESPONSE   = 0x40,   /*!< The Enhanced Add Scene Response command allows a device to respond to an Enhanced Add Scene command.*/
+    ESP_ZB_ZCL_CMD_SCENES_ENHANCED_VIEW_SCENE_RESPONSE  = 0x41,   /*!< The Enhanced View Scene Response command allows a device to respond to an Enhanced View Scene command 
+                                                                        using a finer scene transition time that was available in the ZCL. */
+    ESP_ZB_ZCL_CMD_SCENES_COPY_SCENE_RESPONSE           = 0x42,   /*!< The Copy Scene Response command allows a device to respond to a Copy Scene command.*/
+} esp_zb_zcl_scenes_cmd_resp_id_t;
+
 #ifdef __cplusplus
 }
 #endif
