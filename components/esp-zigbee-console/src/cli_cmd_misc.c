06937753cb7f1044934deb36174217312a3eb017
Pengyu Lv <lvpengyu@espressif.com>
2025-02-07 10:55:44 +0800

    feat(console): add commands to dump neighbor & route table
    Signed-off-by: Pengyu Lv <lvpengyu@espressif.com>

--- components/esp-zigbee-console/src/cli_cmd_misc.c
+++ components/esp-zigbee-console/src/cli_cmd_misc.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -136,6 +137,67 @@ static esp_err_t cli_macfilter_clear(esp_zb_cli_cmd_t *self, int argc, char *arg
     return ESP_OK;
 }
 
+/* Implementation of "neighbor table" command */
+
+static esp_err_t cli_neighbor_table(esp_zb_cli_cmd_t *self, int argc, char **argv)
+{
+    static const char *titles[] = {"Index", "Age", "NwkAddr", "MacAddr", "Type", "Rel", "Depth", "LQI", "Cost"};
+    static const uint8_t widths[] = {5, 5, 8, 20, 5, 3, 5, 5, 6};
+    static const char *dev_type_name[] = {
+        [ESP_ZB_DEVICE_TYPE_COORDINATOR] = "ZC",
+        [ESP_ZB_DEVICE_TYPE_ROUTER]      = "ZR",
+        [ESP_ZB_DEVICE_TYPE_ED]          = "ZED",
+        [ESP_ZB_DEVICE_TYPE_NONE]        = "UNK",
+    };
+    static const char rel_name[] = {
+        [ESP_ZB_NWK_RELATIONSHIP_PARENT]                = 'P', /* Parent */
+        [ESP_ZB_NWK_RELATIONSHIP_CHILD]                 = 'C', /* Child */
+        [ESP_ZB_NWK_RELATIONSHIP_SIBLING]               = 'S', /* Sibling */
+        [ESP_ZB_NWK_RELATIONSHIP_NONE_OF_THE_ABOVE]     = 'O', /* Others */
+        [ESP_ZB_NWK_RELATIONSHIP_PREVIOUS_CHILD]        = 'c', /* Previous Child */
+        [ESP_ZB_NWK_RELATIONSHIP_UNAUTHENTICATED_CHILD] = 'u', /* Unauthenticated Child */
+    };
+    esp_zb_nwk_info_iterator_t itor = ESP_ZB_NWK_INFO_ITERATOR_INIT;
+    esp_zb_nwk_neighbor_info_t neighbor = {};
+
+    cli_output_table_header(ARRAY_SIZE(widths), titles, widths);
+    while (ESP_OK == esp_zb_nwk_get_next_neighbor(&itor, &neighbor)) {
+        cli_output("| %3d | %3d | 0x%04hx | 0x%016" PRIx64 " |",
+                    itor, neighbor.age, neighbor.short_addr, *(uint64_t *)neighbor.ieee_addr);
+        cli_output(" %3s | %c |", dev_type_name[neighbor.device_type], rel_name[neighbor.relationship]);
+        cli_output(" %3d | %3d |  o:%d |", neighbor.depth, neighbor.lqi, neighbor.outgoing_cost);
+        cli_output("\n");
+    }
+
+    return ESP_OK;
+}
+
+/* Implementation of "route table" command */
+
+static esp_err_t cli_route_table(esp_zb_cli_cmd_t *self, int argc, char **argv)
+{
+    static const char *titles[] = {"Index", "DestAddr", "NextHop", "Expiry", "State", "Flags"};
+    static const uint8_t widths[] = {5, 8, 8, 6, 8, 6};
+    static const char *route_state_name[] = {
+        [ESP_ZB_NWK_ROUTE_STATE_ACTIVE] = "Active",
+        [ESP_ZB_NWK_ROUTE_STATE_DISCOVERY_UNDERWAY] = "Disc",
+        [ESP_ZB_NWK_ROUTE_STATE_DISCOVERY_FAILED] = "Fail",
+        [ESP_ZB_NWK_ROUTE_STATE_INACTIVE] = "Inactive",
+    };
+    esp_zb_nwk_info_iterator_t itor = ESP_ZB_NWK_INFO_ITERATOR_INIT;
+    esp_zb_nwk_route_info_t route = {};
+
+    cli_output_table_header(ARRAY_SIZE(widths), titles, widths);
+    while (ESP_OK == esp_zb_nwk_get_next_route(&itor, &route)) {
+        cli_output("| %3d | 0x%04hx%c| 0x%04hx | %4d | %6s | 0x%02x |",
+                    itor, route.dest_addr, route.flags.group_id ? 'g' : ' ', route.next_hop_addr,
+                    route.expiry, route_state_name[route.flags.status], *(uint8_t *)&route.flags);
+        cli_output("\n");
+    }
+
+    return ESP_OK;
+}
+
 DECLARE_ESP_ZB_CLI_CMD(factoryreset, cli_factoryreset,, "Reset the device to factory new immediately");
 DECLARE_ESP_ZB_CLI_CMD(reboot,       cli_reboot,,       "Reboot the device immediately");
 DECLARE_ESP_ZB_CLI_CMD(radio,        cli_radio,,        "Enable/Disable the radio");
@@ -144,4 +206,10 @@ DECLARE_ESP_ZB_CLI_CMD(trace,        cli_trace,,        "Configure Zigbee stack 
 DECLARE_ESP_ZB_CLI_CMD_WITH_SUB(macfilter, "Zigbee stack mac filter management",
     ESP_ZB_CLI_SUBCMD(add,      cli_macfilter_add,      "Add device ieee addr for filter in"),
     ESP_ZB_CLI_SUBCMD(clear,    cli_macfilter_clear,    "Clear all entries in the filter"),
+);
+DECLARE_ESP_ZB_CLI_CMD_WITH_SUB(neighbor, "Neighbor information",
+    ESP_ZB_CLI_SUBCMD(table,    cli_neighbor_table,     "Dump the neighbor table on current node."),
+);
+DECLARE_ESP_ZB_CLI_CMD_WITH_SUB(route, "Route information",
+    ESP_ZB_CLI_SUBCMD(table,    cli_route_table,        "Dump the route table in current node."),
 );
