8b31bc212147ef46b9266095d255b98187237dd9
PiotrWQR <6188@akademiazamojska.edu.pl>
2025-04-28 23:38:28 +0200

    12

--- examples/esp_zigbee_customized_devices/customized_client/main/esp_HA_customized_switch.h
+++ examples/esp_zigbee_customized_devices/customized_client/main/esp_HA_customized_switch.h
@@ -5,6 +5,7 @@
  *
  * Zigbee customized client Example
  *
+ * 
  * This example code is in the Public Domain (or CC0 licensed, at your option.)
  *
  * Unless required by applicable law or agreed to in writing, this
