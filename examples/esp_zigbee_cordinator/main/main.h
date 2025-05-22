350a59bb67204401145950cfa54e40896308a041
PiotrWQR <6188@akademiazamojska.edu.pl>
2025-05-22 22:43:38 +0200

    move up

--- examples/esp_zigbee_cordinator/main/main.h
+++ examples/esp_zigbee_cordinator/main/main.h
@@ -10,9 +10,9 @@
 #define ESP_MANUFACTURER_NAME "\x09""ESPRESSIF"      /* Customized manufacturer name */
 #define ESP_MODEL_IDENTIFIER "\x07"CONFIG_IDF_TARGET /* Customized model identifier */
 
-#define ESP_ZB_ZR_CONFIG()                                                              \
+#define ESP_ZB_ZR_CONFIG()                                                          \
 {                                                                                   \
-    .esp_zb_role = ESP_ZB_DEVICE_TYPE_COORDINATOR,                                       \
+    .esp_zb_role = ESP_ZB_DEVICE_TYPE_COORDINATOR,                                  \
     .install_code_policy = INSTALLCODE_POLICY_ENABLE,                               \
     .nwk_cfg.zczr_cfg = {                                                           \
         .max_children = MAX_CHILDREN,                                               \
@@ -20,12 +20,12 @@
 }
 
 #define ESP_ZB_DEFAULT_RADIO_CONFIG()                           \
-{                                                           \
-    .radio_mode = ZB_RADIO_MODE_NATIVE,                     \
+{                                                               \
+    .radio_mode = ZB_RADIO_MODE_NATIVE,                         \
 }
 
 #define ESP_ZB_DEFAULT_HOST_CONFIG()                            \
-{                                                           \
-    .host_connection_mode = ZB_HOST_CONNECTION_MODE_NONE,   \
+{                                                               \
+    .host_connection_mode = ZB_HOST_CONNECTION_MODE_NONE,       \
 }
 /* Zigbee configuration */
