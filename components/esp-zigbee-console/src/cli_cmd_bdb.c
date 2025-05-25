4a4ec2d9885d693654421c541012617cc5e7028d
Michal Jirků <box@wejn.org>
2025-05-25 19:10:55 +0200

    Add support for link key management to zb console
    This adds new command `linkkey` (with subcommands `set`, `add`, `delete`)
    to manage link key configuration in `components/esp-zigbee-console`.
    
    Part of #659 (TZ-1820).

--- components/esp-zigbee-console/src/cli_cmd_bdb.c
+++ components/esp-zigbee-console/src/cli_cmd_bdb.c
@@ -483,6 +483,101 @@ exit:
     return ret;
 }
 
+/* Sub-commands of `linkkey` */
+
+typedef enum linkkey_op_s {
+    LKO_add,
+    LKO_remove,
+    LKO_set,
+} linkkey_op_t;
+
+static esp_err_t cli_linkkey_op(linkkey_op_t op, esp_zb_cli_cmd_t *self, int argc, char **argv)
+{
+    struct {
+        arg_str_t *type;
+        arg_hex_t *key;
+        arg_lit_t *help;
+        arg_end_t *end;
+    } argtable = {
+        .type = arg_str0("t", "type", "<type:d|c>", "link key type: d: distributed, c: default global TC link key), default: c"),
+        .key = arg_hexn(NULL, NULL, "<key128:KEY>", 1, 1, "link key, in HEX format"),
+        .help = arg_lit0(NULL, "help", "Print this help message"),
+        .end = arg_end(2),
+    };
+    esp_err_t ret = ESP_OK;
+    bool centralized = true; // true if type: c
+
+    /* Parse command line arguments */
+    int nerrors = arg_parse(argc, argv, (void**)&argtable);
+    EXIT_ON_FALSE(argtable.help->count == 0, ESP_OK, arg_print_help((void**)&argtable, argv[0]));
+    EXIT_ON_FALSE(nerrors == 0, ESP_ERR_INVALID_ARG, arg_print_errors(stdout, argtable.end, argv[0]));
+
+    EXIT_ON_FALSE(argtable.key->hsize[0] == 16, ESP_ERR_INVALID_ARG);
+
+    if (argtable.type->count > 0) {
+        switch (argtable.type->sval[0][0]) {
+            case 'd':
+            case 'D':
+                centralized = false;
+                break;
+            case 'c':
+            case 'C':
+                centralized = true;
+                break;
+            default:
+                EXIT_ON_ERROR(ESP_ERR_INVALID_ARG, cli_output("%s: invalid argument to option --type\n", argv[0]));
+                break;
+        }
+    }
+
+    if (centralized) {
+        switch (op) {
+            case LKO_set:
+                esp_zb_secur_TC_standard_preconfigure_key_set(argtable.key->hval[0]);
+                break;
+            case LKO_add:
+                ret = esp_zb_secur_multi_TC_standard_preconfigure_key_add(argtable.key->hval[0]);
+                break;
+            case LKO_remove:
+                ret = esp_zb_secur_multi_TC_standard_preconfigure_key_remove(argtable.key->hval[0]);
+                break;
+        }
+    } else {
+        switch (op) {
+            case LKO_set:
+                esp_zb_secur_TC_standard_distributed_key_set(argtable.key->hval[0]);
+                break;
+            case LKO_add:
+                ret = esp_zb_secur_multi_standard_distributed_key_add(argtable.key->hval[0]);
+                break;
+            case LKO_remove:
+                ret = esp_zb_secur_multi_standard_distributed_key_remove(argtable.key->hval[0]);
+                break;
+        }
+    }
+
+exit:
+    arg_hex_free(argtable.key);
+    ESP_ZB_CLI_FREE_ARGSTRUCT(&argtable);
+    return ret;
+}
+
+static esp_err_t cli_linkkey_add(esp_zb_cli_cmd_t *self, int argc, char **argv)
+{
+    return cli_linkkey_op(LKO_add, self, argc, argv);
+}
+
+static esp_err_t cli_linkkey_remove(esp_zb_cli_cmd_t *self, int argc, char **argv)
+{
+    return cli_linkkey_op(LKO_remove, self, argc, argv);
+}
+
+static esp_err_t cli_linkkey_set(esp_zb_cli_cmd_t *self, int argc, char **argv)
+{
+    return cli_linkkey_op(LKO_set, self, argc, argv);
+}
+
+
 /* Sub-commands of `ic` */
 
 static esp_err_t cli_ic_policy(esp_zb_cli_cmd_t *self, int argc, char **argv)
@@ -938,6 +1033,11 @@ DECLARE_ESP_ZB_CLI_CMD_WITH_SUB(network, "Network configuration",
     ESP_ZB_CLI_SUBCMD(close,    cli_nwk_close,    "Close local network"),
     ESP_ZB_CLI_SUBCMD(scan,     cli_nwk_scan,     "Scan for network"),
     ESP_ZB_CLI_SUBCMD(ed_scan,  cli_nwk_ed_scan,  "Scan for energy detect on channels"),
+);
+DECLARE_ESP_ZB_CLI_CMD_WITH_SUB(linkkey, "Link Key Configuration",
+    ESP_ZB_CLI_SUBCMD(add,    cli_linkkey_add,     "Add additional global link key"),
+    ESP_ZB_CLI_SUBCMD(remove, cli_linkkey_remove,  "Remove additional global link key"),
+    ESP_ZB_CLI_SUBCMD(set,    cli_linkkey_set,     "Set default global link key"),
 );
 DECLARE_ESP_ZB_CLI_CMD_WITH_SUB(ic, "Install code configuration",
     ESP_ZB_CLI_SUBCMD(policy, cli_ic_policy, "Set install code policy"),
